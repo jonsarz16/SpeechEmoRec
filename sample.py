4a523df42fcfa70ee143d4003f5f2a4d46551f19
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-01-11 15:04:42 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -27,6 +27,6 @@ uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 #     resized = mobilenet_v2_preprocess_input(resized)
 #     img_reshape = resized[np.newaxis,...]
 CLASSIFY = st.button("Generate Prediction")    
-    if CLASSIFY:
+if CLASSIFY:
 #         prediction = model.predict(img_reshape).argmax()
-       st.title("the result is")
+    st.title("the result is")
