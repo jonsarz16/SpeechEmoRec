b3dd95d03b06e14ab4694b79cbdf6e63525e5b10
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-01-11 15:06:46 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,11 +5,11 @@ st.title("Speech Emotion Recognition")
 # import cv2
 import numpy as np
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+# import tensorflow as tf
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-# model = tf.keras.models.load_model("saved_model/mdl_wts.hdf5")
+# # model = tf.keras.models.load_model("saved_model/mdl_wts.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
