6496aa8c973c12730a6bfcdc9232417869aafd92
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-01-15 16:06:25 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -24,6 +24,7 @@ if file_audio is not None:
     sample, srate = librosa.load(file_audio)
     mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
     mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)  #power_to_db = amplitude squared to decibel units
+    st.echo(mel_spect)
     # img_input
 CLASSIFY = st.button("Generate Prediction")    
 if CLASSIFY:
