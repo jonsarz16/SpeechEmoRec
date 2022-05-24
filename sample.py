15e411eafd134628abc39d582b0f7c9100e7b3da
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 13:21:55 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,9 +10,9 @@ selected = option_menu(
     default_index=0,
     orientation="horizontal",
     styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "container": {"padding": "0!important", "background-color": "#2a368c"},
+        "icon": {"color": "orange", "font-size": "15px"}, 
+        "nav-link": {"font-size": "17px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "green"},
     }
 )
