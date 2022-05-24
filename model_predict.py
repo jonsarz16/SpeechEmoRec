bfe4994b50b7c93b45ddeb1caa035c648ec23968
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 18:56:56 +0800

    Update model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -17,7 +17,7 @@ def model_predict(audiofile):
     plt.ylabel("Amplitude")
     plt.show()  
     plt.savefig('waveplot.png')
-    st.image('waveplot.png', caption='Audio waveplot',width=50)
+    st.image('waveplot.png', caption='Audio waveplot',width=75)
     # createWaveplot(sample,srate)
     # mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
     # mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)  #power_to_db = amplitude squared to decibel units
