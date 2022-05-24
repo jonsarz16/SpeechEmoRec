e1de9e86cb48d756cb69eb5df142a89f61391bfc
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 14:09:17 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ selected = option_menu(
     default_index=0,
     orientation="horizontal",
     styles={
-        "container": {"padding": "0!important", "background-color": "#a1b5d6"},
+        "container": {"padding": "0!important", "background-color": "#a1b5d6", "display: flex"},
         "icon": {"color": "black", "font-size": "30px"}, 
         "nav-link": {"font-size": "17px", "text-align": "auto", "margin":"0px", "--hover-color": "#ffe100"},
         "nav-link-selected": {"background-color": "green"},}
