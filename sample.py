0dac318a4b784317c9244302a2e2d36f2ce247ad
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 14:17:41 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -17,7 +17,7 @@ selected = option_menu(
     styles={
         "container": {"padding": "0!important", "background-color": "#a1b5d6", "display": "inline"},
         "icon": {"color": "black", "font-size": "22px"}, 
-        "nav-link": {"font-size": "17px", "text-align": "auto", "margin":"0px", "--hover-color": "#ffe100"},
+        "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#ffe100"},
         "nav-link-selected": {"background-color": "green"},}
 )
 
