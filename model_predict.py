9f17129bd8fdaa9ce0ab6138262f50ef5e515b43
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 18:14:20 +0800

    Update model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -1,13 +1,19 @@
 from preprocess_augment import *
-
+def createWaveplot(sample, sr):
+  plt.figure()
+  librosa.display.waveplot(sample, sr)
+  plt.title("Waveplot for audio")
+  plt.xlabel("Time (seconds)")
+  plt.ylabel("Amplitude")
+  plt.show()
 
 def model_predict(audiofile):
-
     sample, srate = librosa.load(audiofile)
+    createWaveplot(sample,srate)
     mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
     mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)  #power_to_db = amplitude squared to decibel units
     mel_spect1 = cv2.resize(mel_spect, (256, 256))
-   
+    
     mel_spect1 = mel_spect1.astype("float")
     mel_spect1 = np.array(mel_spect1)
     input = np.expand_dims(mel_spect1,axis=0) #single photo
