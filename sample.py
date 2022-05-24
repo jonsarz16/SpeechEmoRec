62131c1d84eb6eb0e3cbf5900a19546ca5de77b0
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 13:19:37 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 selected = option_menu(
     menu_title="Main Menu",
     options=["Improved Algo", "Baseline", "Performance Comparison"],
-    icons=["house","book","envelope"],
+    icons=["arrow-up-right-circle","arrow-repeat","app"],
     menu_icon="cast",
     default_index=0,
     orientation="horizontal",
