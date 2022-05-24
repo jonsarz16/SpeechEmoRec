969f4de74638f8a2bd2a04c4fff77c5a3e756240
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-24 13:59:11 +0800

    edit

--- model_predict.py
+++ model_predict.py
@@ -1,28 +1,4 @@
-import tensorflow as tf
-from keras.models import load_model
-import tensorflow.keras as keras
 from preprocess_augment import *
-
-def main(model):
-    # load models
-    model = load_model(model)
-
-    ### load file
-    file_audio = st.file_uploader("", type=['mp3','wav'])
-
-    if file_audio is not None:
-        CLASSIFY = st.button("Generate Prediction")
-         # preprocess the audio file
-        input = model_predict(file_audio)
-    else:
-        CLASSIFY = st.button("Generate Prediction on Test File")
-    # preprocess the audio file
-        audio_file = open('YAF_back_angry.wav', 'rb')
-        input = model_predict(audio_file)
-    
-    if CLASSIFY:
-        output = model.predict(input)
-        st.write(output)
 
 
 def model_predict(audiofile):
--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu 
 from model_predict import *
@@ -5,7 +6,7 @@ import tensorflow as tf
 from keras.models import load_model
 import tensorflow.keras as keras
 
-
+st.header("Speech Emotion Recognition")
 selected = option_menu(
     None,
     options=["Improved Algo", "Baseline", "Performance Comparison"],
@@ -17,15 +18,29 @@ selected = option_menu(
         "container": {"padding": "0!important", "background-color": "#a1b5d6"},
         "icon": {"color": "black", "font-size": "30px"}, 
         "nav-link": {"font-size": "17px", "text-align": "auto", "margin":"0px", "--hover-color": "#ffe100"},
-        "nav-link-selected": {"background-color": "green"},
-    }
+        "nav-link-selected": {"background-color": "green"},}
 )
 
-# if selected == "Improved Algo":
-#     # st.title(f"You have selected {selected}")
 if selected == "Baseline":
     st.write(f"You have selected {selected}")
-    mymodel = load_model("model.hdf5")
-    main(mymodel)
-# if selected == "Performance Comparison":
+    # load models
+    model = load_model("model.hdf5")
+
+    # load file
+    file_audio = st.file_uploader("", type=['mp3','wav'])
+
+    if file_audio is not None:
+        CLASSIFY = st.button("Generate Prediction")
+    # preprocess the audio file
+        input = model_predict(file_audio)
+    else:
+        CLASSIFY = st.button("Generate Prediction on Test File")
+    # preprocess the audio file
+        audio_file = open('YAF_back_angry.wav', 'rb')
+        input = model_predict(audio_file)
     
+    
+    
+    if CLASSIFY:
+    output = model.predict(input)
+    st.write(output)
