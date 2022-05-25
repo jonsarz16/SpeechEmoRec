08e9b73a935787109e748c65c02e0a2bbe3b4d5f
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-25 20:49:12 +0800

    Update model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -5,7 +5,10 @@ from keras.models import load_model
 def run_model(modeltype):
     file_audio = st.file_uploader("", type=['mp3','wav'])
     if file_audio is not None:
+      if modeltype == "Baseline":
         data_visual_baseline(file_audio)
+      else:
+        
 
 
 
@@ -43,6 +46,44 @@ def data_visual_baseline(audiofile):
   # mel_spect1 = np.array(mel_spect1)
   # input = np.expand_dims(mel_spect1,axis=0)
   # input1 = input[:,:,:,np.newaxis]
+
+def data_visual_improved(audiofile):
+  
+  sample, srate = librosa.load(audiofile)
+  plt.figure()
+  librosa.display.waveplot(sample, srate)
+  plt.title("Waveplot for audio")
+  plt.xlabel("Time (seconds)")
+  plt.ylabel("Amplitude")
+  plt.show()  
+  plt.savefig('waveplots.png',dpi = 70)
+  st.image('waveplots.png', caption=' ')
+    
+  FIG_SIZE = (10,5)
+  hop_length = 512 #stride
+  n_fft = 2048 #num. of samples per window
+  stft = librosa.stft(sample, n_fft=n_fft, hop_length=hop_length)
+  
+  
+  # mel_spectrogram
+  plt.figure(figsize=FIG_SIZE)
+  librosa.display.specshow(spectrogram, sr=srate, hop_length=hop_length)
+  plt.xlabel("Time")
+  plt.ylabel("Frequency")
+  # plt.colorbar()
+  plt.title("Spectrogram")
+  plt.savefig('specs.png',transparent=True,dpi = 50)
+  st.image('specs.png', caption=' ')
+   
+  # mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
+  # mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)
+  # mel_spect1 = cv2.resize(mel_spect, (256, 256))
+    
+  # mel_spect1 = mel_spect1.astype("float")
+  # mel_spect1 = np.array(mel_spect1)
+  # input = np.expand_dims(mel_spect1,axis=0)
+  # input1 = input[:,:,:,np.newaxis]
+
 
 
 
