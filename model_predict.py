94423f729cfc4b981a407d15aa727f469738d488
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-05-25 16:17:37 +0800

    Update model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -1,12 +1,5 @@
-from preprocess_augment import *
 import streamlit as st
-def createWaveplot(sample, sr):
-  plt.figure()
-  librosa.display.waveplot(sample, sr)
-  plt.title("Waveplot for audio")
-  plt.xlabel("Time (seconds)")
-  plt.ylabel("Amplitude")
-  plt.show()
+from preprocess_augment import *
 
 def model_predict(audiofile):
     sample, srate = librosa.load(audiofile)
@@ -32,14 +25,14 @@ def model_predict(audiofile):
     plt.title("Spectrogram")
     plt.savefig('specs.png',transparent=True,dpi = 50)
     st.image('specs.png', caption=' ')
-    # createWaveplot(sample,srate)
-    # mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
-    # mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)  #power_to_db = amplitude squared to decibel units
-    # mel_spect1 = cv2.resize(mel_spect, (256, 256))
+   
+    mel_spectrogram = librosa.feature.melspectrogram(sample, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=256)
+    mel_spect = librosa.power_to_db(mel_spectrogram, ref=np.max)
+    mel_spect1 = cv2.resize(mel_spect, (256, 256))
     
-    # mel_spect1 = mel_spect1.astype("float")
-    # mel_spect1 = np.array(mel_spect1)
-    # input = np.expand_dims(mel_spect1,axis=0) #single photo
-    # input1 = input[:,:,:,np.newaxis] #single photo
-    # return input1
+    mel_spect1 = mel_spect1.astype("float")
+    mel_spect1 = np.array(mel_spect1)
+    input = np.expand_dims(mel_spect1,axis=0)
+    input1 = input[:,:,:,np.newaxis]
+    st.write(model.predict(input1))
 
