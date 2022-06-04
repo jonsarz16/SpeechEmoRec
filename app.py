ee2d58d1975f5a80f7fea0af9b90b997924099b8
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-06-04 19:28:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ if selected == "Baseline":
                 st.title("Prediction Results")
                 result = classify('melspecs.png')
                 predicted_emo = emotions[result]
-                st.write("**Predicted Emotion**: ",predicted_emo.upper())
+                st.write(result)
+                # st.write("**Predicted Emotion**: ",predicted_emo.upper())
                 # scores = tf.nn.softmax(result[0]).numpy()
                 # st.write(scores)
         except:
