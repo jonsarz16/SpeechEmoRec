4a7ccb8fd31b9d83a26a7b8fcbc6b9e778860aa1
jonsarz16 <53893506+jonsarz16@users.noreply.github.com>
2022-06-04 13:50:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ st.set_page_config(
  )
 
 #baseline model
-test_model = load_model("model.hdf5")
+test_model = load_model("baseline500e.hdf5")
 
 # improved_model
 # improved_model = load_model("improved.hdf5")
